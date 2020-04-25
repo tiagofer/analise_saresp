9da105d194c0885e0ef0439b828ea3c1b7737122
Tiago Ferreira <tiago.ferreira@2mi.com.br>
2020-04-25 12:19:19 -0300

    criação de página de proficiência

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from helpers import read_markdown_file, analise_ano, analise_ano,fetch_data_csv
+from helpers import read_markdown_file, analise_ano, analise_ano,fetch_data_csv, \
+show_line,show_bar
 
 menu = ['Introdução','Entendendo os dados','Total Escolas entre 2011 e 2018',
 'Proficiência por disciplina', 'Próximos passos e estudos futuros']
@@ -19,8 +20,14 @@ def main():
     if menu_option == menu[2]:
         texto_escolas = read_markdown_file('textos/analise_escolas.md')
         st.markdown(texto_escolas,unsafe_allow_html=True)
-        analise_ano(df_saresp,'arquivo','CODESC')
+        analise_ano(df_saresp,'arquivo','NOMESC')
     if menu_option == menu[3]:
-        st.markdown('# pizza')
+        prof_intro = read_markdown_file('textos/proficiencia_intro.md')
+        st.markdown(prof_intro,unsafe_allow_html=True)
+        st.markdown('# Média das notas por disciplina ao longo dos anos')
+        show_line(df_saresp,'arquivo','medprof','ds_comp')
+        st.markdown('Na análise acima, observa-se uma evolução nas notas médias de Português e matemática.')
+        st.markdown('Isolando-se as duas disciplinas, vamos agora observar o comportamento destas notas conforme a série.')
+        show_bar(df_saresp)
 if __name__ == "__main__":
     main()
---
+++ textos/proficiencia_intro.md
@@ -0,0 +1,9 @@
+# Níveis de proficiência SARESP
+
+Os níveis de proficiência do SARESP seguem a escala proposta pelo SAEB e pela Prova Brasil, representando os resultados da proficiência dos alunos em todo o percurso da educação básica.
+
+A pontuação em cada nível é definida a partir das expectativas de aprendizagem (conteúdo, habilidades e competências) estabelecidas para cada ano ou série e pelo componente curricular no Currículo do Estado de São Paulo.
+
+A interpretação dessa escala é feita de forma cumulativa, ou seja, os alunos que estão em um determinado nível dominam não só as habilidades associadas à esse nível, mas também as proficiências descritas nos níveis anteriores.
+
+Abaixo, vamos fazer algumas análises sobre o comportamento das notas de proficiência.
